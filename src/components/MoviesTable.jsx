dced38de479c2c8e44d6293f8c1a35702f69102a
dnsamw <dnsam.mit@gmail.com>
2022-08-26 00:02:50 +0530

    1 file modified - create genres list group

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 
 class MoviesTable extends Component {
   state = {
     movies: getMovies(),
+    genres: getGenres(),
   };
 
   handleLike = (movie) => {
@@ -20,51 +22,72 @@ class MoviesTable extends Component {
     this.setState({ movies: filteredMovies });
   };
 
+  handleSelectGenre = (genre) => {
+    console.log(genre);
+  };
+
   render() {
-    const { movies } = this.state;
+    const { movies, genres } = this.state;
     return (
       <>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <i
-                    className={
-                      movie.liked
-                        ? 'fa fa-heart clickable'
-                        : 'fa fa-heart-o clickable'
-                    }
-                    aria-hidden="true"
-                    onClick={() => this.handleLike(movie)}
-                  ></i>
-                </td>
-                <button
-                  onClick={() => this.handleDelete(movie)}
-                  style={{ backgroundColor: 'red', color: 'white' }}
-                  type="button"
-                  className="btn btn-sm m-1"
+        <div className="row mt-2">
+          <div className="col-3">
+            <ul className="list-group">
+              {genres.map((genre) => (
+                <li
+                  onClick={() => this.handleSelectGenre(genre)}
+                  key={genre._id}
+                  className="list-group-item clickable"
                 >
-                  Delete
-                </button>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                  {genre.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Stock</th>
+                  <th scope="col">Rate</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((movie) => (
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <i
+                        className={
+                          movie.liked
+                            ? 'fa fa-heart clickable'
+                            : 'fa fa-heart-o clickable'
+                        }
+                        aria-hidden="true"
+                        onClick={() => this.handleLike(movie)}
+                      ></i>
+                    </td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      style={{ backgroundColor: 'red', color: 'white' }}
+                      type="button"
+                      className="btn btn-sm m-1"
+                    >
+                      Delete
+                    </button>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </>
     );
   }
