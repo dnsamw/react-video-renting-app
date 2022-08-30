6a1c62f475b5066eddaf8c2c54d5338e9fff01d8
dnsamw <dnsam.mit@gmail.com>
2022-08-30 13:20:36 +0530

    2 files modified - imput component (controlled)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
 
-function Login() {
-  return <h1>Login</h1>;
+import Input from './common/Input';
+
+class Login extends Component {
+  state = {
+    account: { username: '', password: '' },
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const { name, value } = input;
+    const account = { ...this.state.account };
+    account[name] = value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Login</h1>
+        <form>
+          <Input
+            name="username"
+            label="username"
+            value={this.state.account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={this.state.account.password}
+            onChange={this.handleChange}
+          />
+        </form>
+      </>
+    );
+  }
 }
 
 export default Login;
---
+++ src/components/common/Input.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  render() {
+    const { name, label, value, onChange, type = 'text', error } = this.props;
+    return (
+      <>
+        <div className="for-group">
+          <label htmlFor={name}>{label}</label>
+          <input
+            name={name}
+            value={value}
+            onChange={onChange}
+            type={type}
+            id={name}
+            className="form-control"
+          />
+        </div>
+        <div className="alert alert-danger mt-1 p1">{error}</div>
+      </>
+    );
+  }
+}
+
+export default Input;
